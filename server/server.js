3c444b794557e3478e5f4bfd6d093c6beee5bfab
Honzaless <149673967+Honzaless@users.noreply.github.com>
2024-05-14 22:50:22 +0200

    changed cookie on login to samesite none

--- server/server.js
+++ server/server.js
@@ -55,7 +55,7 @@ app.post("/login", async (req, res) => {
     const isPass = bcrypt.compareSync(password, foundUser.password);
     if (isPass) {
       jwt.sign({userId: foundUser._id, username}, jwtSecretKey, {}, (err, token) => {
-        res.cookie("token", token).json({
+        res.cookie("token", token, {sameSite: "none", secure: "true"}).json({
           id: foundUser._id,
         })
       });
