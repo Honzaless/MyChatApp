1870dc867e4f2112e71a12e68b6d29faf8249abc
Honzaless <149673967+Honzaless@users.noreply.github.com>
2024-05-15 13:01:25 +0200

    made a little change to logout - hopefully should fix the issue

--- server/server.js
+++ server/server.js
@@ -99,7 +99,7 @@ app.post("/select", async (req, res) => {
 app.use(fileUpload());
 
 app.post("/logout", (req, res) => {
-  res.clearCookie("token");
+  res.clearCookie("token", {path: "/");
   res.end();
 });
 
